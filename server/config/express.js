88bbedac913718c505b2bff359c93a1b6858b9b4
zenddignite <nikiviliev98@gmail.com>
2018-03-31 17:16:23 +0300

    add new security middlewares

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,9 @@ const session = require('express-session')
 const passport = require('passport')
 const handlebars = require('express-handlebars')
 const helmet = require('helmet')
-var csrf = require('csurf') // Cross-site Request Forgery Protection
+const csrf = require('csurf') // Cross-site Request Forgery Protection
+const encrypt = require('../utilities/encryption')
+const RateLimit = require('express-rate-limit')
 
 module.exports = (app) => {
   app.engine('handlebars', handlebars({
@@ -17,7 +19,11 @@ module.exports = (app) => {
   app.use(bodyParser.json()) // for parsing application/json
   app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
   app.use(session({
-    secret: 'neshto-taino!@#$%',
+    secret: encrypt.generateSalt(),
+    cookie: {
+      maxAge: 2628000000,
+      secure: 'auto'
+    },
     resave: false,
     saveUninitialized: false,
     httpOnly: true
@@ -26,8 +32,14 @@ module.exports = (app) => {
   app.use(passport.session())
 
   app.use(csrf()) // Cross-site Request Forgery Protection
+  app.use(new RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+  }))
 
   app.use((req, res, next) => {
+    // console.log(req.session.passport)
     if (req.user) {
       res.locals.currentUser = req.user
       res.locals.isAdmin = req.user.roles.indexOf('Admin') >= 0
